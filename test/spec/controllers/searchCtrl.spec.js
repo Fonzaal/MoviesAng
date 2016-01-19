b5606512805b10ec862e8f5a566ddf17b9438fa3
alfonso <al.cas89@gmail.com>
2016-01-19 17:03:42 -0600

    -Added searchCtrl unit testing.

--- test/spec/controllers/searchCtrl.spec.js
+++ test/spec/controllers/searchCtrl.spec.js
@@ -5,17 +5,25 @@ describe('Controller: searchCtrl', function () {
   // load the controller's module
   beforeEach(module('moviesApp'));
 
-  var controller,
-    scope;
+  var controller, scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
+    //Create a new child scope for the controller.
     scope = $rootScope.$new();
-    controller = $controller('MainCtrl', {
+    //Create a new instance of the searchCtrl.
+    controller = $controller('searchCtrl', {
       $scope: scope
       // place here mocked dependencies
     });
   }));
 
+  it('should have movie defined', function(){
+    expect(scope.movie).toBeDefined();
+  });
+
+  it('should have moviesList defined', function(){
+    expect(scope.moviesList).toBeDefined();
+  });
 
 });
