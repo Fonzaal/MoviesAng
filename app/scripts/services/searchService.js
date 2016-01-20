f440fa57d66193bdf0b6f9d4082f69dfc74ff47c
alfonso <al.cas89@gmail.com>
2016-01-20 17:36:16 -0600

    -Added cache service.

--- app/scripts/services/searchService.js
+++ app/scripts/services/searchService.js
@@ -8,57 +8,101 @@
  */
 
 angular.module('moviesApp')
-  .factory('searchFactory',function($http, $q, $log){
+  .factory('searchFactory',function($http, $q, $log, $cacheFactory){
     var searchFactory = {};
+    var cache = $cacheFactory.get('$http');
 
     searchFactory.getMovieResults = function (title){
       var deferred = $q.defer();
-      $http.get('http://www.omdbapi.com/?s=' + title)
+      var url = 'http://www.omdbapi.com/?s=' + title;
+      var cacheData = cache.get(url);
+
+      if(cacheData){
+        $log.log('got data from cache');
+        deferred.resolve(angular.fromJson(cacheData[1]).Search);
+      }
+      else {
+        $http.get(url , {
+            cache: true
+          })
           .success(function(data){
-            $log.log(data.Search);
+            $log.log('got data from webservice')
             deferred.resolve(data.Search);
           })
           .error(function(data){
             deferred.reject('Error');
           });
+      }
       return deferred.promise;
     };
 
     searchFactory.getMovieDetails = function (id){
       var deferred = $q.defer();
-      $http.get('http://www.omdbapi.com/?i='+id)
-        .success(function(data){
-          //$log.log(data);
-          deferred.resolve(data);
-        })
-        .error(function(){
-          deferred.reject('Error');
-        });
+      var url = 'http://www.omdbapi.com/?i=';
+      var cacheData = cache.get(url + id);
+
+      if(cacheData){
+        $log.log('got data from cache');
+        deferred.resolve(angular.fromJson(cacheData[1]));
+      }
+      else {
+        $http.get(url + id , {
+            cache: true
+          })
+          .success(function(data){
+            $log.log('got data from webservice');
+            deferred.resolve(data);
+          })
+          .error(function(){
+            deferred.reject('Error');
+          });
+      }
       return deferred.promise;
     };
 
     searchFactory.getEpisodes = function (id, season){
       var deferred = $q.defer();
-      $http.get('http://www.omdbapi.com/?i=' + id + '&Season='+season)
-        .success(function(data){
-          //$log.log(data);
-          deferred.resolve(data);
-        })
-        .error(function(){
-          deferred.reject('Error');
-        });
+      var url = 'http://www.omdbapi.com/?i=' + id + '&Season=' + season;
+      var cacheData = cache.get(url);
+
+      if(cacheData){
+        $log.log('got data from cache');
+        deferred.resolve(angular.fromJson(cacheData[1]));
+      }
+      else {
+        $http.get(url , {
+            cache: true
+          })
+          .success(function(data){
+            $log.log('got data from webservice');
+            deferred.resolve(data);
+          })
+          .error(function(){
+            deferred.reject('Error');
+          });
+      }
       return deferred.promise;
     };
+
     searchFactory.getEpisodeDetails = function (id, season, episode){
       var deferred = $q.defer();
-      $http.get('http://www.omdbapi.com/?i=' + id + '&Season=' + season + '&Episode=' + episode)
-        .success(function(data){
-          $log.log(data);
-          deferred.resolve(data);
-        })
-        .error(function(){
-          deferred.reject('Error');
-        });
+      var url = 'http://www.omdbapi.com/?i=' + id + '&Season=' + season + '&Episode=' + episode;
+      var cacheData = cache.get(url);
+
+      if(cacheData){
+        $log.log('got data from cache');
+        deferred.resolve(angular.fromJson(cacheData[1]));
+      }
+      else {
+        $http.get(url)
+          .success(function(data){
+            $log.log('got data from webservice');
+            deferred.resolve(data);
+          })
+          .error(function(){
+            deferred.reject('Error');
+          });
+      }
       return deferred.promise;
     };
 
